08badb7e9c142a4d1779fcd3ce6913372407a6a3
slhs <amalinic22@stlouishighschool.ie>
2025-09-24 10:33:23 +0100

    Tetris

---
+++ game.py
@@ -0,0 +1,29 @@
+import pygame, sys
+from grid import Grid
+from blocks import *
+
+pygame.init()
+purple = (240, 100, 227)
+
+screen = pygame.display.set_mode((300, 600))
+pygame.display.set_caption("Tetris!")
+
+clock = pygame.time.Clock()
+
+game_grid = Grid()
+
+block = LBlock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+    screen.fill(purple)
+    game_grid.draw(screen)
+    block.draw(screen)
+
+    pygame.display.update()
+    clock.tick(60)
